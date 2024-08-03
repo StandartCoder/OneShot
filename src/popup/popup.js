405487f28c324372862039eb9eceb1f3835ae704
StandartCoder <marc.peater@icloud.com>
2024-08-03 14:32:44 +0200

    👻 added home/settings pane and caching

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,12 +1,31 @@
 import { getCache, setCache } from '../utils/cache.js';
 import { getSettings, setSettings } from '../utils/settings.js';
-import { getThemeData, setTheme, getTheme } from '../utils/theme.js';
 import { getLangData, getCurrentLang, listLangs, setLang } from '../utils/lang.js';
 
 document.addEventListener('DOMContentLoaded', async function () {
     const tabs = document.querySelectorAll('.tab');
     const contents = document.querySelectorAll('.content');
+
+    const homeIcon = document.getElementById('homeIcon');
+    homeIcon.addEventListener('click', () => {
+        tabs.forEach(t => t.classList.remove('active'));
+        contents.forEach(content => content.classList.remove('active'));
+
+        const cache = getCache();
+        cache.lastPane = null;
+        setCache(cache);
+
+        document.getElementById('paneHome').classList.add('active');
+    });
     
+    const settingsIcon = document.getElementById('settingsIcon');
+    settingsIcon.addEventListener('click', () => {
+        tabs.forEach(t => t.classList.remove('active'));
+        contents.forEach(content => content.classList.remove('active'));
+
+        document.getElementById('paneSettings').classList.add('active');
+    });
+
     tabs.forEach(tab => {
         tab.addEventListener('click', () => {
             tabs.forEach(t => t.classList.remove('active'));
@@ -16,6 +35,10 @@ document.addEventListener('DOMContentLoaded', async function () {
                 content.classList.remove('active');
                 if(content.id === tab.dataset.tab) {
                     content.classList.add('active');
+
+                    let cache = getCache();
+                    cache.lastPane = tab.dataset.tab;
+                    setCache(cache);
                 }
             });
 
@@ -54,6 +77,29 @@ document.addEventListener('DOMContentLoaded', async function () {
                 element.innerText = langData[key];
             }
         });
+
+        const cache = getCache(); let tab;
+        if (cache.lastPane) {
+            if (cache.lastPane === 'panePreset') {
+                tab = tabs[0];
+            }
+
+            if (cache.lastPane === 'paneCopyPaste') {
+                tab = tabs[1];
+            }
+
+            if (cache.lastPane === 'paneMore') {
+                tab = tabs[2];
+            }
+
+            tab.classList.add('active');
+            contents.forEach(content => {
+                content.classList.remove('active');
+                if(content.id === tab.dataset.tab) {
+                    content.classList.add('active');
+                }
+            });
+        }
 
         document.getElementById('loaderOverlay').classList.remove('active');
     }, 750);
